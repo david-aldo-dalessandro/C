b4a35f240a3e35851a99dc3ce03197baf4369391
David D'Alessandro <ddalessandro1@gulls.salisbury.edu>
2021-11-14 16:33:42 -0600

    First commit

---
+++ ch1s1/ch1s1.c
@@ -0,0 +1,13 @@
+/*David D'Alessandro
+ *11/14/21
+ *Practice problems for chapter 1 section 1 of 'The C Language'
+ */
+
+#include <stdio.h>
+
+int main(){
+
+  printf("Hello, World\n");
+  
+  return 0;
+}
